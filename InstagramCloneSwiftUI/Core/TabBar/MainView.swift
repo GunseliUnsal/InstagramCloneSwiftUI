88462e2c617bef80ff8fd04f5a44d6a77894dbe6
Gunseli Unsal <gunseliunsal@gmail.com>
2024-09-27 19:38:03 +0300

    add tab bar setup

---
+++ InstagramCloneSwiftUI/Core/TabBar/MainView.swift
@@ -0,0 +1,36 @@
+//
+//  MainView.swift
+//  InstagramCloneSwiftUI
+//
+//  Created by Günseli Ünsal on 27.09.2024.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView {
+            Tab("", systemImage: "house") {
+                Text("Feed")
+            }
+            Tab("", systemImage: "magnifyingglass") {
+                Text("Search")
+            }
+            Tab("", systemImage: "plus.square") {
+                Text("Upload")
+            }
+            Tab("", systemImage: "heart") {
+                Text("Liked")
+            }
+            Tab("", systemImage: "person") {
+                Text("Profile")
+            }
+        }
+        .tint(.primary)
+        
+    }
+}
+
+#Preview {
+    MainView()
+}
