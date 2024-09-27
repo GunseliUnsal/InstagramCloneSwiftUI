930da519932e5e210f9589e0f3c03f16c72faa0f
Gunseli Unsal <gunseliunsal@gmail.com>
2024-09-27 18:53:28 +0300

    Initial Commit

---
+++ InstagramCloneSwiftUI/App/InstagramCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneSwiftUIApp.swift
+//  InstagramCloneSwiftUI
+//
+//  Created by Günseli Ünsal on 27.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
