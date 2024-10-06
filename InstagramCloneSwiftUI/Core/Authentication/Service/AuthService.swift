15a1815039a376342b98a2cf53d88de4499414d3
Gunseli Unsal <gunseliunsal@gmail.com>
2024-10-06 22:43:44 +0300

    add upload user data to Firestore

--- InstagramCloneSwiftUI/Core/Authentication/Service/AuthService.swift
+++ InstagramCloneSwiftUI/Core/Authentication/Service/AuthService.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import FirebaseAuth
+import FirebaseFirestore
 
 class AuthService {
     @Published var userSession: FirebaseAuth.User?
@@ -17,6 +18,7 @@ class AuthService {
         self.userSession = Auth.auth().currentUser
     }
     
+    @MainActor
     func login(withEmail email: String, password: String) async throws {
         do {
             let result = try await Auth.auth().signIn(withEmail: email, password: password)
@@ -26,10 +28,12 @@ class AuthService {
         }
     }
     
+    @MainActor
     func createUser(withEmail email: String, password: String, username: String) async throws {
         do {
             let result = try await Auth.auth().createUser(withEmail: email, password: password)
             self.userSession = result.user
+            await uploadUserData(uid: result.user.uid, username: username, email: email)
         } catch {
             print("DEBUG: Failed to register user with error: \(error.localizedDescription)")
         }
@@ -40,6 +44,12 @@ class AuthService {
     func signout () {
         try? Auth.auth().signOut()
         self.userSession = nil
+    }
+    
+    private func uploadUserData(uid: String, username: String, email: String) async {
+        let user = User(id: uid, username: username, email: email)
+        guard let encodedUser = try? Firestore.Encoder().encode(user) else { return }
+        try? await Firestore.firestore().collection("users").document(user.id).setData(encodedUser)
     }
     
 }
